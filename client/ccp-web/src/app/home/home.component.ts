3b74c31d42193d547f33de1a70687d83280e051b
Patrick Palmer <ppalmer@pm.me>
2020-09-06 23:39:27 -0500

    Tweak register form for Netlify support

--- client/ccp-web/src/app/home/home.component.ts
+++ client/ccp-web/src/app/home/home.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpParams, HttpClient } from '@angular/common/http';
+import { VirtualGuest } from './shared/virtual-guest.model';
 
 @Component({
   selector: 'ccp-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() { }
+export class HomeComponent {
+  virtualGuest: VirtualGuest = {
+    firstName: '',
+    lastName: '',
+    zoomEmail: '',
+  };
 
-  ngOnInit(): void {
+  constructor(public http: HttpClient) {}
+
+  onSubmit(zoomRegistration) {
+    if (!zoomRegistration.valid) {
+      console.log(this.virtualGuest);
+
+      return;
+    }
+
+    const body = new HttpParams()
+      .set('form-name', 'zoom-register')
+      .append('firstName', this.virtualGuest.firstName)
+      .append('lastName', this.virtualGuest.lastName)
+      .append('zoomEmail', this.virtualGuest.zoomEmail);
+
+    this.http
+      .post('/', body.toString(), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
-
 }
---
+++ client/ccp-web/src/app/home/shared/virtual-guest.model.ts
@@ -0,0 +1,7 @@
+export class VirtualGuest {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public zoomEmail: string
+  ) {}
+}
